373caec07f1d7405469766817c15240cc861163f
Unknown <manuel.strenge@hotmail.com>
2018-03-07 15:59:56 +0100

    Added random

--- src/ch/bbw/model/TSFormulas.java
+++ src/ch/bbw/model/TSFormulas.java
@@ -3,7 +3,9 @@ package ch.bbw.model;
 import javafx.geometry.Point2D;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Random;
 
 public class TSFormulas {
     private List<Point2D>points;
@@ -12,7 +14,15 @@ public class TSFormulas {
         points = new ArrayList<>();
     }
 
-    public void setRandompath(){
+    public void setRandompath(ArrayList<Point2D> points){
+        Random random =new Random();
+        ArrayList<Point2D>pointTemps = (ArrayList<Point2D>) points.clone();
+        while (pointTemps.size()!=0){
+            int index=random.nextInt(pointTemps.size());
+            points.add(pointTemps.get(index));
+            points.remove(index);
+            points.trimToSize();
+        }
 
     }
 }
